1f666db0e006e50aa85884b65511b46d67d44373
shraddhapal27 <palshraddha41@gmail.com>
2025-12-17 14:57:18 +0530

    [Shraddha Pal] : Add two sum program

---
+++ gcr-codebase/programming-prerequisites/AddTwoNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class AddTwoNumber{
+	public static void main(String args[]){
+	    Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+}
