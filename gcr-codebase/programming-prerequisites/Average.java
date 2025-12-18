0b20969061bf6d8c8e9452005a7a75f5b9bdebc4
shraddhapal27 <palshraddha41@gmail.com>
2025-12-18 11:54:37 +0530

    [Shraddha Pal] : Added average of numbers program

---
+++ gcr-codebase/programming-prerequisites/Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Average{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+	float avg = (a+b+c)/3;
+	System.out.println(avg);
+  }
+}
