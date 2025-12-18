b840d2308ba7f2d3a8e15523977c3636cff29b23
shraddhapal27 <palshraddha41@gmail.com>
2025-12-18 11:52:08 +0530

    [Shraddha Pal] : Added unit conversion program

---
+++ gcr-codebase/programming-prerequisites/KmToM.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class KmToM{
+ public static void main(String args[]){
+   Scanner sc = new Scanner(System.in);
+   double distance = sc.nextDouble();
+   double miles = distance*0.621371;
+   System.out.println(miles);
+ }
+}
