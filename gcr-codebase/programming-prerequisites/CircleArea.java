46b8eea36b3bead10379a8ec385a5930a9e04bbe
shraddhapal27 <palshraddha41@gmail.com>
2025-12-18 11:53:46 +0530

    [Shraddha Pal] : Added area of circle program

---
+++ gcr-codebase/programming-prerequisites/CircleArea.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CircleArea{
+  public static void main(String args[]){
+   Scanner sc = new Scanner(System.in);
+     int radius = sc.nextInt();
+     	  int areaOfCircle = (22/7)*radius^2;
+	         System.out.println(areaOfCircle);
+  }
+}
