65d85e04a964259dc84c14319c880cdcba30f525
shraddhapal27 <palshraddha41@gmail.com>
2025-12-19 23:04:19 +0530

    [Shraddha Pal] : Added Kilometres to Miles Conversion program

---
+++ gcr-codebase/programming-elements/Level1-practice-problems/KilometresToMilesConversion.java
@@ -0,0 +1,12 @@
+// create a class to convert distance of certain kilometres into miles
+class KilometresToMilesConversion{
+  public static void main(String args[]){
+    
+	double distance = 10.8;
+	
+	// create a variable named distanceInMiles and calculate its value
+	double distanceInMiles = 1.6 * 10.8;
+	
+	System.out.print(distanceInMiles);
+  }
+}
