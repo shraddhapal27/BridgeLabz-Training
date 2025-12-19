44a74a1c8ca4b713859eef3fc2288f969f8f0a37
shraddhapal27 <palshraddha41@gmail.com>
2025-12-19 23:06:12 +0530

    [Shraddha Pal] :Added average of subjects program

---
+++ gcr-codebase/programming-elements/Level1-practice-problems/SubjectsAverage.java
@@ -0,0 +1,20 @@
+// Create a class  calculating average marks of three subjects
+class SubjectsAverage{
+  public static void main(String args[]){
+  
+  // create a variable maths and assign value 94 to it 
+		int maths = 94;
+		
+  // create a variable maths and assign value 94 to it
+		int physics = 95;
+		
+  // create a variable maths and assign value 94 to it
+		int chemistry = 96;
+		
+  // create a variable average to indicate the average 
+        float average = (maths + physics + chemistry)/3;
+		
+  // print the average details 
+        System.out.println(average);
+  }
+}
