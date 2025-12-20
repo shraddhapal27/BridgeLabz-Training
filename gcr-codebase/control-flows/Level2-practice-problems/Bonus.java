20a1dd34ac19ac6620464d2f898e502628a20afa
shraddhapal27 <palshraddha41@gmail.com>
2025-12-20 12:04:01 +0530

    [Shraddha Pal] : Added bonus program

---
+++ gcr-codebase/control-flows/Level2-practice-problems/Bonus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Bonus{
+  public static void main(String args[]){
+   Scanner sc = new Scanner(System.in);
+   
+   double salary = sc.nextDouble();
+   int year = sc.nextInt();
+   double bonus;
+   if(year > 5){
+     bonus = salary*0.05;
+	 System.out.print("Bonus Amount: " + bonus);
+   }else{
+     System.out.print("No bonus. Service is 5 years or less");
+    }
+   
+   sc.close();
+  }
+}
