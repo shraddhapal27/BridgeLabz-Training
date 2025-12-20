7700df3d40fe68e82589bb0c1060198d9cdf00f2
shraddhapal27 <palshraddha41@gmail.com>
2025-12-20 09:27:04 +0530

    [Shraddha Pal] : Added AbundantNumber program

---
+++ gcr-codebase/control-flows/Level3-practice-problems/AbundantNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AbundantNumber {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int sum = 0;
+
+        // Find sum of divisors
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        // Check abundant condition
+        if (sum > number) {
+            System.out.println("Abundant Number");
+        } else {
+            System.out.println("Not an Abundant Number");
+        }
+
+        sc.close();
+    }
+}
+
