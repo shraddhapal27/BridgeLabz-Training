0d966d1ed46f687e01915650cba7c77f6160915e
shraddhapal27 <palshraddha41@gmail.com>
2025-12-21 16:23:10 +0530

    [Shraddha Pal] : Added largest digits program

---
+++ gcr-codebase/Arrays/Level2-practice-problems/LargestDigits.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class LargestDigits{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+	
+	int number = sc.nextInt();
+	int maxDigit = 10;
+	int[] digits = new int[10];
+	int index = 0;
+	
+	while(number != 0 && index < maxDigit){
+	    int digits = number % 10;
+        digits[index++] = digits;
+        number /= 10;        		
+	 }
+	 
+	 int largest = 0, secondLargest = 0;
+	 for(int i=0; i<index; i++){
+	 
+	   if(digits[i]) > largest{
+	     secondLargest = largest;
+		 largest = digits[i];
+	   }else if(digits[i] > secondLargest && digits[i] != largest){
+	     secondLargest = digits[i];
+	   }
+	 }
+	
+	 System.out.println("Largest = " + largest);
+     System.out.println("Second Largest = " + secondLargest);
+	 
+	 sc.close();
+   }
+}
