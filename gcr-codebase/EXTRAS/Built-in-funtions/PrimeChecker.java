592b8a88cf60a36346049ae6f21acc416f1f4ef0
shraddhapal27 <palshraddha41@gmail.com>
2025-12-23 23:42:59 +0530

    [Shraddha Pal] : Added PrimeChecker program

---
+++ gcr-codebase/EXTRAS/Built-in-funtions/PrimeChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+
+    static boolean isPrime(int num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println(num + (isPrime(num) ? " is Prime" : " is Not Prime"));
+    }
+}
