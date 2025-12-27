8dbfd1fe678c102d5ea8836b8825a18cc1c0cbae
shraddhapal27 <palshraddha41@gmail.com>
2025-12-27 23:58:40 +0530

    [Shraddha Pal] : Updated CarRental logic

--- java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/CarRental.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/CarRental.java
@@ -1,5 +1,39 @@
 package com.constructor.level1;
 
 public class CarRental {
+    String customerName;
+    String carModel;
+    int rentalDays;
+    double totalCost;
+    
+    CarRental(){
+    	customerName = "Guest";
+        carModel = "Hatchback";
+        rentalDays = 1;
+        calculateCost();
+    }
+    
+    
+     // Parameterized Constructor
+    CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+        calculateCost();
+    }
 
+    // Method to calculate total cost
+    void calculateCost() {
+        double costPerDay = 1500;
+        totalCost = rentalDays * costPerDay;
+    }
+
+    // Method to display rental details
+    void displayRental() {
+        System.out.println("Customer Name : " + customerName);
+        System.out.println("Car Model     : " + carModel);
+        System.out.println("Rental Days   : " + rentalDays);
+        System.out.println("Total Cost    : ₹" + totalCost);
+        System.out.println("-----------------------------");
+    }
 }
--- java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/UseCarRental.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/UseCarRental.java
@@ -1,5 +1,14 @@
 package com.constructor.level1;
 
 public class UseCarRental {
+	 public static void main(String[] args) {
 
+	        // Using default constructor
+	        CarRental r1 = new CarRental();
+	        r1.displayRental();
+
+	        // Using parameterized constructor
+	        CarRental r2 = new CarRental("Shraddha", "Sedan", 3);
+	        r2.displayRental();
+	    }
 }
