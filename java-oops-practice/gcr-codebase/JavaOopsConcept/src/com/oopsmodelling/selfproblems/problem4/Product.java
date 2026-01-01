4bf1accc0f1d9690ed2d7a37e230bb0f8eed1e8d
shraddhapal27 <palshraddha41@gmail.com>
2026-01-01 14:41:46 +0530

    [Shraddha Pal] : Added Product program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/oopsmodelling/selfproblems/problem4/Product.java
@@ -0,0 +1,17 @@
+package com.oopsmodelling.selfproblems.problem4;
+
+class Product {
+    int productId;
+    String productName;
+    double price;
+
+    Product(int productId, String productName, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    void displayProduct() {
+        System.out.println(productName + " - ₹" + price);
+    }
+}
