71e9f7283c5a68dba9928e9553d8a96c04e0b079
shraddhapal27 <palshraddha41@gmail.com>
2026-01-02 00:05:33 +0530

    [Shraddha Pal] : Added AnimalHierarchyDemo program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/assisstedproblems/problem1/AnimalHierarchyDemo.java
@@ -0,0 +1,14 @@
+package com.inheritance.assisstedproblems.problem1;
+
+public class AnimalHierarchyDemo {
+    public static void main(String[] args) {
+
+        Animal a1 = new Dog("Buddy", 3);
+        Animal a2 = new Cat("Whiskers", 2);
+        Animal a3 = new Bird("Tweety", 1);
+
+        a1.makeSound();
+        a2.makeSound();
+        a3.makeSound();
+    }
+}
