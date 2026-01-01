f48cb3ae18a60fcd56844b5ea473f7a8b4b35a88
shraddhapal27 <palshraddha41@gmail.com>
2026-01-02 00:12:36 +0530

    [Shraddha Pal] : Added CheckingAccount program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hierarchialinheritance/problem1/CheckingAccount.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchialinheritance.problem1;
+
+class CheckingAccount extends BankAccount {
+    double withdrawalLimit;
+
+    CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    @Override
+    void displayAccountType() {
+        System.out.println("Account Type     : Checking Account");
+        System.out.println("Withdrawal Limit : ₹" + withdrawalLimit);
+    }
+}
