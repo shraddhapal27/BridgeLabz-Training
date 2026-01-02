d4a0873d5a6f35dccdc621c191b3d9798a9091ea
shraddhapal27 <palshraddha41@gmail.com>
2026-01-02 23:37:03 +0530

    [Shraddha Pal] : Added BankAccount program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulation/problem4/BankAccount.java
@@ -0,0 +1,47 @@
+package com.encapsulation.problem4;
+
+abstract class BankAccount {
+    private String accountNumber;
+    private String holderName;
+    protected double balance;
+
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Encapsulation
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    // Abstract method
+    abstract double calculateInterest();
+}
+
