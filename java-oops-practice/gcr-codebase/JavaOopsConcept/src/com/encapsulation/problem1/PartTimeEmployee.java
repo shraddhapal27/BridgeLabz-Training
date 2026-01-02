01defc3adba17f0417fb259cf2ded7b4fff787fe
shraddhapal27 <palshraddha41@gmail.com>
2026-01-02 23:27:25 +0530

    [Shraddha Pal] : Added PartTimeEmployee program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulation/problem1/PartTimeEmployee.java
@@ -0,0 +1,29 @@
+package com.encapsulation.problem1;
+
+class PartTimeEmployee extends Employee implements Department {
+
+    private int hoursWorked;
+    private double hourlyRate;
+    private String department;
+
+    public PartTimeEmployee(int id, String name, int hoursWorked, double hourlyRate) {
+        super(id, name, 0);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
