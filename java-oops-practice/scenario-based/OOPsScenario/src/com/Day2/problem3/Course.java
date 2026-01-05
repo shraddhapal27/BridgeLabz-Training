74c38b31c9485b0fed85ae63acf28f565199ffdb
shraddhapal27 <palshraddha41@gmail.com>
2026-01-05 12:19:38 +0530

    [Shraddha Pal] : Added Course program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day2/problem3/Course.java
@@ -0,0 +1,21 @@
+package com.Day2.problem3;
+
+class Course {
+    private String courseCode;
+    private String courseName;
+    private int credits;
+
+    public Course(String code, String name, int credits) {
+        this.courseCode = code;
+        this.courseName = name;
+        this.credits = credits;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
