4e5fc7b5eae0eacd277d9ca36d60ac92a020a97e
shraddhapal27 <palshraddha41@gmail.com>
2026-01-05 16:26:08 +0530

    [Shraddha Pal] : Added FriendNode program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/singlelinkedlist/problem3/FriendNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist.singlelinkedlist.problem3;
+
+public class FriendNode {
+	 int friendId;
+	    FriendNode next;
+
+	    FriendNode(int friendId) {
+	        this.friendId = friendId;
+	        this.next = null;
+	    }
+}
