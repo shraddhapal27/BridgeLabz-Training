f955672599ea37f926e4ec0b72dd0b80f17e3f34
shraddhapal27 <palshraddha41@gmail.com>
2026-01-05 16:25:04 +0530

    [Shraddha Pal] : Added MovieNode program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/singlelinkedlist/problem2/MovieNode.java
@@ -0,0 +1,20 @@
+package com.linkedlist.singlelinkedlist.problem2;
+
+
+class MovieNode {
+    String title;
+    String director;
+    int year;
+    double rating;
+    MovieNode next;
+    MovieNode prev;
+
+    MovieNode(String title, String director, int year, double rating) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+        this.next = null;
+        this.prev = null;
+    }
+}
