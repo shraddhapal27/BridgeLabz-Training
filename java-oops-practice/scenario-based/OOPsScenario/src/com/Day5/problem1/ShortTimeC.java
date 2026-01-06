399afc83d126b5156ea52bea109753c1ef99d1db
shraddhapal27 <palshraddha41@gmail.com>
2026-01-07 00:02:29 +0530

    [Shraddha Pal] : Added ShortTimeC program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day5/problem1/ShortTimeC.java
@@ -0,0 +1,7 @@
+package com.Day5.problem1;
+
+public class ShortTimeC implements ICertifiable{
+	public void generateCertificate() {
+        System.out.println("Short-Time Course Professional Certificate Generated");
+    }
+}
