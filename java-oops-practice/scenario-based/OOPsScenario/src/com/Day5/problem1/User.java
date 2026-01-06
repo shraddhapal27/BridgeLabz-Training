7fb3bf1c945af741034b7ef68f35998483483f87
shraddhapal27 <palshraddha41@gmail.com>
2026-01-07 00:02:40 +0530

    [Shraddha Pal] : Added User program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day5/problem1/User.java
@@ -0,0 +1,13 @@
+package com.Day5.problem1;
+
+public class User {
+    String name;
+    String email;
+    int userId;
+    
+    User(String name, String email, int id){
+    	this.name = name;
+    	this.email = email;
+    	this.userId = userId;
+    }
+}
