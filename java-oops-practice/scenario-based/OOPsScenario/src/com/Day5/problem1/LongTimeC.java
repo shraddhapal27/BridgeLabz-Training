6ea86acf24ff6096b9544d6e66ed5b2461670dbf
shraddhapal27 <palshraddha41@gmail.com>
2026-01-07 00:01:55 +0530

    [Shraddha Pal] : Added LongTimeC program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day5/problem1/LongTimeC.java
@@ -0,0 +1,7 @@
+package com.Day5.problem1;
+
+public class LongTimeC implements ICertifiable{
+	public void generateCertificate() {
+        System.out.println("Full-Time Course Professional Certificate Generated");
+    }
+}
