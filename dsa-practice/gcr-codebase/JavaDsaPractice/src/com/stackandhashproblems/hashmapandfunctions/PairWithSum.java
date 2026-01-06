f6a43aef2a9e367d068933a12eab87b18b2d4b3e
shraddhapal27 <palshraddha41@gmail.com>
2026-01-06 17:00:20 +0530

    [Shraddha Pal] : Added PairWithSum program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stackandhashproblems/hashmapandfunctions/PairWithSum.java
@@ -0,0 +1,24 @@
+package com.stackandhashproblems.hashmapandfunctions;
+
+import java.util.HashSet;
+
+class PairWithSum {
+    public static boolean hasPair(int[] arr, int target) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(target - num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        System.out.println(hasPair(arr, target));
+    }
+}
