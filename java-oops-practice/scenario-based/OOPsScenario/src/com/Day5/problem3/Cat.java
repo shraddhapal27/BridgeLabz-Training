8f23d2741316f0b59cb15b44a2b672c3d3d62e48
shraddhapal27 <palshraddha41@gmail.com>
2026-01-06 23:56:16 +0530

    [Shraddha Pal] : Added Cat program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day5/problem3/Cat.java
@@ -0,0 +1,12 @@
+package com.Day5.problem3;
+
+class Cat extends Pet {
+    public Cat(String name, int age) {
+        super(name, "Cat", age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Meow! 🐱");
+    }
+}
