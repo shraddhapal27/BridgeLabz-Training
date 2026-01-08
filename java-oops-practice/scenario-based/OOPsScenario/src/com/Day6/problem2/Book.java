59f0779167168a62d00edf7fa8f8520299ab1856
shraddhapal27 <palshraddha41@gmail.com>
2026-01-08 13:16:36 +0530

    [Shraddha Pal] : Added Book program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day6/problem2/Book.java
@@ -0,0 +1,34 @@
+package com.Day6.problem2;
+
+public class Book implements IDiscountable {
+
+	 protected String title;
+	 protected String author;
+	 protected double price;
+	 private int stock;          // encapsulated
+
+	 // Constructor
+	 public Book(String title, String author, double price, int stock) {
+	     this.title = title;
+	     this.author = author;
+	     this.price = price;
+	     this.stock = stock;
+	 }
+
+	 // Reduce stock safely
+	 public void reduceStock(int qty) {
+	     if (qty <= stock) {
+	         stock = stock - qty;
+	     }
+	 }
+
+	 // Get stock
+	 public int getStock() {
+	     return stock;
+	 }
+
+	 // Default discount (no discount)
+	 public double applyDiscount(int quantity) {
+	     return 0;
+	 }
+	}
