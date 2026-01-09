b692232acbe36165bceaff248ab2c14bb3a8a4b2
shraddhapal27 <palshraddha41@gmail.com>
2026-01-09 11:48:49 +0530

    [Shraddha Pal] : Added Hotel program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day7/problem2/Hotel.java
@@ -0,0 +1,14 @@
+package com.Day7.problem2;
+
+class Hotel {
+    private double cost;
+
+    Hotel(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
+
