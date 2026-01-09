384a7e0f5d4b9cc5cfd3e50ed812a67e634aae70
shraddhapal27 <palshraddha41@gmail.com>
2026-01-09 11:48:26 +0530

    [Shraddha Pal] : Added Activity program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day7/problem2/Activity.java
@@ -0,0 +1,13 @@
+package com.Day7.problem2;
+
+class Activity {
+    private double cost;
+
+    Activity(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
