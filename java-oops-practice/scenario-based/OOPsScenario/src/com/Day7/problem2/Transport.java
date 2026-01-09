c919f2e679d3057e2252ac701cc3e0c36e4d68d2
shraddhapal27 <palshraddha41@gmail.com>
2026-01-09 11:52:29 +0530

    [Shraddha Pal] : Added Transport program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day7/problem2/Transport.java
@@ -0,0 +1,13 @@
+package com.Day7.problem2;
+
+class Transport {
+    private double cost;
+
+    Transport(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
