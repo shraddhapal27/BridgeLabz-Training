4df32e696f401b1bf32d50f74199d2922f0d9e81
shraddhapal27 <palshraddha41@gmail.com>
2026-01-11 23:02:47 +0530

    [Shraddha Pal] : Added User program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day8/problem3/User.java
@@ -0,0 +1,11 @@
+package com.Day8.problem3;
+
+class User {
+    protected String name;
+    protected String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
