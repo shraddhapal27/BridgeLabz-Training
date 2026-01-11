954ef8361bc2da431eca3a3276c7218983e5541f
shraddhapal27 <palshraddha41@gmail.com>
2026-01-11 22:56:15 +0530

    [Shraddha Pal] : Added NonVegItem program

---
+++ java-oops-practice/scenario-based/OOPsScenario/src/com/Day8/problem2/NonVegItem.java
@@ -0,0 +1,7 @@
+package com.Day8.problem2;
+
+class NonVegItem extends FoodItem {
+    public NonVegItem(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+}
