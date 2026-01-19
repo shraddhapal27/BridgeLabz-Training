1eedbd770c1d9ce3eba8c244410006d9adce0aff
shraddhapal27 <palshraddha41@gmail.com>
2026-01-19 12:08:27 +0530

    [Shraddha Pal] : Added map interface practice questions

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/JavaCollections/mapinterface/MaxValueKey.java
@@ -0,0 +1,26 @@
+package com.JavaCollections.mapinterface;
+
+import java.util.*;
+
+public class MaxValueKey {
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = Map.of(
+            "A", 10,
+            "B", 20,
+            "C", 15
+        );
+
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            if (e.getValue() > maxValue) {
+                maxValue = e.getValue();
+                maxKey = e.getKey();
+            }
+        }
+
+        System.out.println(maxKey);
+    }
+}
---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/JavaCollections/mapinterface/WordFrequency.java
@@ -0,0 +1,21 @@
+package com.JavaCollections.mapinterface;
+
+import java.util.*;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+
+        String text = "Hello world, hello Java!";
+
+        Map<String, Integer> map = new HashMap<>();
+
+        text = text.toLowerCase().replaceAll("[^a-z ]", "");
+        String[] words = text.split("\\s+");
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
