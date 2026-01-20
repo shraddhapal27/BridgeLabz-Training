a577c772c2c04713d4476bafcd802b417bfcbf6b
shraddhapal27 <palshraddha41@gmail.com>
2026-01-20 11:19:20 +0530

    [Shraddha Pal] : Added EduResults program

---
+++ dsa-practice/scenario-based/DsaScenarioProblems/src/com/Day7/EduResults.java
@@ -0,0 +1,58 @@
+package com.Day7;
+
+public class EduResults {
+
+    // Merge two sorted parts
+    static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) { // stability maintained
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1)
+            arr[k++] = L[i++];
+
+        while (j < n2)
+            arr[k++] = R[j++];
+    }
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void display(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] marks = {78, 90, 85, 90, 60, 75};
+
+        mergeSort(marks, 0, marks.length - 1);
+        display(marks);
+    }
+}
