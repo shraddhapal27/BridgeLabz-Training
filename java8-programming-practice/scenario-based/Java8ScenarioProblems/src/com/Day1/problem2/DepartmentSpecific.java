653c16ab6a4443215253efcf15f57621d4fc71ce
Shraddha <168215842+shraddhapal27@users.noreply.github.com>
2026-02-06 14:46:08 +0530

    Delete java8-programming-practice/scenario-based/Java8ScenarioProblems/src/com/Day1 directory

--- java8-programming-practice/scenario-based/Java8ScenarioProblems/src/com/Day1/problem2/DepartmentSpecific.java
+++
@@ -1,9 +0,0 @@
-package com.Day1.problem2;
-
-import java.util.List;
-
-public class DepartmentSpecific {
-    public static void main(String[] args) {
-    	List<Student> list = StudentData.getStudent()
-    }
-}
--- java8-programming-practice/scenario-based/Java8ScenarioProblems/src/com/Day1/problem2/SpecificQueres.java
+++
@@ -1,7 +0,0 @@
-package com.Day1.problem2;
-
-public class SpecificQueres {
-    public static void main(String[] args) {
-    	
-    }
-}
