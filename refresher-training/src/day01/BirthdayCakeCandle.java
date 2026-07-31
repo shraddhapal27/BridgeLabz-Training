ca8d06a2bbc4975882ccc661d29fb86e986db9ad
shraddhapal27 <palshraddha41@gmail.com>
2026-07-31 19:43:29 +0530

    [Shraddha Pal] : Added cake candle program

--- refresher-training/src/day01/BirthdayCakeCandle.java
+++ refresher-training/src/day01/BirthdayCakeCandle.java
@@ -1,4 +1,25 @@
 package day01;
 
+import java.util.Scanner;
+
 public class BirthdayCakeCandle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] height = {3,2,1,3};
+        int max = height[0];
+        int count = 0;
+        for(int i=0; i<height.length; i++){
+            if(height[i] > max){
+                max = height[i];
+            }
+        }
+
+        for(int i=0; i<height.length; i++){
+            if(height[i] ==  max){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
