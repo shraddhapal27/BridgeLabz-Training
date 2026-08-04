e2746a01978bd9bf8541c320341ea892a7377f5a
shraddhapal27 <palshraddha41@gmail.com>
2026-08-04 08:33:25 +0530

    [Shraddha Pal] : Added ArrayStack program'

--- refresher-training/src/day02/ArrayStack.java
+++ refresher-training/src/day02/ArrayStack.java
@@ -1,4 +1,61 @@
 package day02;
 
+class Stack{
+    int[] arr;
+    int top;
+    int capacity;
+
+    public Stack(int size){
+        capacity = size;
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    public void push(int value){
+        if(top == capacity-1){
+            System.out.println("Stack is full");
+            return;
+        }
+        top++;
+        arr[top] = value;
+    }
+
+    public int pop(){
+        if(top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int popValue = arr[top];
+        top--;
+        return popValue;
+    }
+
+    public int peek(){
+        if(top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    public void display(){
+      if(top == -1){
+          System.out.println("Stack is empty");
+      }
+      for(int i = top; i>=0; i--){
+          System.out.println(arr[i] + " ");
+      }
+    }
+}
 public class ArrayStack {
+    public static void main(String[] args) {
+        Stack s = new Stack(6);
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.pop();
+        s.peek();
+        s.display();
+    }
 }
