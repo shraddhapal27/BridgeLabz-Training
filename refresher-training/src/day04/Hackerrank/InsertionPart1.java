b6774480d697f732c0cfcccf28827cba6e007679
shraddhapal27 <palshraddha41@gmail.com>
2026-08-05 23:19:08 +0530

    [Shraddha Pal] : Added InsertionPart1 program

--- refresher-training/src/day04/Hackerrank/InsertionPart1.java
+++ refresher-training/src/day04/Hackerrank/InsertionPart1.java
@@ -1,4 +1,24 @@
 package day04.Hackerrank;
 
+import java.util.List;
+
 public class InsertionPart1 {
+    public static void insertion(int n, List<Integer> arr){
+        int key = arr.get(n-1);
+        int j = n-2;
+        while(j >= 0 && arr.get(j) > key){
+            arr.set(j+1, arr.get(j));
+            for(int num : arr){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            j--;
+        }
+        arr.set(j+1, key);
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
 }
+
