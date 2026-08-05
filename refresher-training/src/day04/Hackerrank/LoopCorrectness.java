2e499c4c92fbffb8a8e01c5795f1cb065bc48e13
shraddhapal27 <palshraddha41@gmail.com>
2026-08-05 23:19:33 +0530

    [Shraddha Pal] : Added LoopCorrectness\ program

--- refresher-training/src/day04/Hackerrank/LoopCorrectness.java
+++ refresher-training/src/day04/Hackerrank/LoopCorrectness.java
@@ -1,4 +1,24 @@
 package day04.Hackerrank;
 
 public class LoopCorrectness {
+    public static void insertionSort(int[] A) {
+        for (int i = 1; i < A.length; i++) {
+            int value = A[i];
+            int j = i - 1;
+            while (j >= 0 && A[j] > value) {
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = value;
+        }
+
+        printArray(A);
+    }
+
+
+    static void printArray(int[] ar) {
+        for (int n : ar) {
+            System.out.print(n + " ");
+        }
+    }
 }
