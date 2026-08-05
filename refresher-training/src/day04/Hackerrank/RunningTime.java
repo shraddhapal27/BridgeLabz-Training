9442711c1b9df43fb2e623f03c7fea5bd1ba4bc8
shraddhapal27 <palshraddha41@gmail.com>
2026-08-05 23:19:58 +0530

    [Shraddha Pal] : Added RunningTime program

--- refresher-training/src/day04/Hackerrank/RunningTime.java
+++ refresher-training/src/day04/Hackerrank/RunningTime.java
@@ -1,4 +1,21 @@
 package day04.Hackerrank;
 
+import java.util.List;
+
 public class RunningTime {
+    public static int runningTime(List<Integer> arr) {
+        // Write your code here
+        int count = 0;
+        for(int i=1; i<arr.size(); i++){
+            int key = arr.get(i);
+            int j = i-1;
+            while(j >= 0 && arr.get(j) > key){
+                arr.set(j + 1, arr.get(j));
+                j--;
+                count++;
+            }
+            arr.set(j+1, key);
+        }
+        return count;
+    }
 }
