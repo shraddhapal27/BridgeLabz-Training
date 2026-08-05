717ef36016352e643b13ffd97173af33317b0440
shraddhapal27 <palshraddha41@gmail.com>
2026-08-05 14:14:39 +0530

    [Shraddha Pal] : Added InsertionSort program

--- refresher-training/src/day04/InsertionSort.java
+++ refresher-training/src/day04/InsertionSort.java
@@ -1,4 +1,21 @@
 package day04;
 
-public class InsertionSort {
+public class InsertionSort
+{
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = j;
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
