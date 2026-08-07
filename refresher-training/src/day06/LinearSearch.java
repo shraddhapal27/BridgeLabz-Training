1770b94a691737b22b2ab6a77bb9d9b082d85ea5
shraddhapal27 <palshraddha41@gmail.com>
2026-08-07 15:00:05 +0530

    [Shraddha Pal] : Added LinearSearch program

--- refresher-training/src/day06/LinearSearch.java
+++ refresher-training/src/day06/LinearSearch.java
@@ -1,4 +1,21 @@
 package day06;
 
 public class LinearSearch {
+    public static int linearSearch(int[] arr, int key){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,8,9};
+       int result =  linearSearch(arr,8);
+        if(result == -1){
+            System.out.println("Element not found");
+        }else{
+            System.out.println("Element found at index " + result);
+        }
+    }
 }
