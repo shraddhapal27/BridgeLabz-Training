90a16535950b3edea42ee715a4beb2241bbc23e1
shraddhapal27 <palshraddha41@gmail.com>
2026-08-07 14:59:26 +0530

    [Shraddha Pal] : Added BinaryFirstOcc program

--- refresher-training/src/day06/BinaryFirstOcc.java
+++ refresher-training/src/day06/BinaryFirstOcc.java
@@ -1,4 +1,30 @@
 package day06;
 
 public class BinaryFirstOcc {
+    public static int firstOccurence(int[] arr,int key){
+        int low = 0, high = arr.length-1, result = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(arr[mid] == key){
+                result = mid;
+                high = mid -1;
+            }
+            else if(arr[mid] < key){
+                low = mid +1;;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 4, 4, 8, 9};
+
+        int first = firstOccurence(arr, 4);
+
+        System.out.println("First occurrence: " + first);
+    }
 }
