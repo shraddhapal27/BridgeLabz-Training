ba594b75bd04b8d53be70baaf4faabbcb632c454
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:32:05 +0530

    [Shraddha Pal] : Added Factorial problem

--- refresher-training/src/day07/Factorial.java
+++ refresher-training/src/day07/Factorial.java
@@ -1,4 +1,13 @@
 package day07;
 
 public class Factorial {
+    public static int factorial(int n){
+        if(n==0 || n==1) return 1;
+
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+      int result =  factorial(4);
+      System.out.println(result);
+    }
 }
