7bfbbaf43fabaf4ed8878cbba1be43dbca356f74
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:33:06 +0530

    [Shraddha Pal] : Added TailRecursion problem

--- refresher-training/src/day07/TailRecursion.java
+++ refresher-training/src/day07/TailRecursion.java
@@ -1,4 +1,15 @@
 package day07;
 
 public class TailRecursion {
+    public static void tailRecursion(int n){
+        if(n==0) return;
+
+        // yaha pehle hm print krte hain fir recursive call hota hai
+        System.out.println(n);
+        tailRecursion(n-1);
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
