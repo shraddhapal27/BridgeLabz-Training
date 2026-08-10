cccdaf21363e6de4e9097d040f7659239267d19f
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:32:33 +0530

    [Shraddha Pal] : Added HeadRecursion problem

--- refresher-training/src/day07/HeadRecursion.java
+++ refresher-training/src/day07/HeadRecursion.java
@@ -1,4 +1,19 @@
 package day07;
 
+
+// tail recursion me logic ke baad koi kaam bacha hi nhi hota.
+// A recursive function is generally called tail recursive when the recursive call is the last operation performed by the function.
+
+// calling and recursive phase
 public class HeadRecursion {
+    public static void headRecursion(int n){
+        if(n==0) return;
+
+        // yaha p pehle function call hota h fir print
+        headRecursion(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+
+    }
 }
