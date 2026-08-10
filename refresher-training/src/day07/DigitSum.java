71f7a6524a79eb0d621b099828e0d6acfcac4e8f
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:31:49 +0530

    [Shraddha Pal] : Added DigitSum problem

--- refresher-training/src/day07/DigitSum.java
+++ refresher-training/src/day07/DigitSum.java
@@ -1,4 +1,12 @@
 package day07;
 
 public class DigitSum {
+    public static int digitSum(int n){
+        if (n==0) return 0;
+        return n%10 + digitSum(n/10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(digitSum(1234));
+    }
 }
