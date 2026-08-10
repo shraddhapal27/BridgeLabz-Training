42ef9c42c37f0bd9e91109ffdf633f689ceb2a9c
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:31:29 +0530

    [Shraddha Pal] : Added V p[roblem

--- refresher-training/src/day07/Count.java
+++ refresher-training/src/day07/Count.java
@@ -1,4 +1,15 @@
 package day07;
 
 public class Count {
+    public static void count(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        count(n-1);
+    }
+
+    public static void main(String[] args) {
+        count(4);
+    }
 }
