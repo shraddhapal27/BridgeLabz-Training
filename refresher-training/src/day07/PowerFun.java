4de08f25639d3a8540a4d5872436dfdab78c964a
shraddhapal27 <palshraddha41@gmail.com>
2026-08-10 13:32:50 +0530

    [Shraddha Pal] : Added PowerFun problem

--- refresher-training/src/day07/PowerFun.java
+++ refresher-training/src/day07/PowerFun.java
@@ -1,4 +1,13 @@
 package day07;
 
 public class PowerFun {
+    public static int power(int base, int exponent){
+        if(exponent == 0) return 1;
+        return base * power(base, exponent-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(5,2));
+
+    }
 }
