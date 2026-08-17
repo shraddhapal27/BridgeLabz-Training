96e37289c73f0907df93532e884eaad85a3108b6
shraddhapal27 <palshraddha41@gmail.com>
2026-08-17 23:33:16 +0530

    [Shraddha pal] : added UseBankAccount program

--- refresher-training/src/day13/homework/UseBankAccount.java
+++ refresher-training/src/day13/homework/UseBankAccount.java
@@ -1,4 +1,9 @@
 package day13.homework;
 
 public class UseBankAccount {
+    public static void main(String[] args) {
+        BankAccount account =
+                new BankAccount(1000, "Savings", "JH01");
+        System.out.println(account.accType);
+    }
 }
