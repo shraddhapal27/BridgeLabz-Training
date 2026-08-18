38df2192ec015a66bb3fea5ae45f4fb6d71e6ffb
shraddhapal27 <palshraddha41@gmail.com>
2026-08-18 14:40:20 +0530

    [Shraddha Pal] : Added Inheritance program

---
+++ refresher-training/src/day14/hackerrank/Inheritance.java
@@ -0,0 +1,17 @@
+//package day14.hackerrank;
+//
+//class Bird extends Animal{
+//    void fly(){
+//        System.out.println("I am flying");
+//    }
+//
+//    void sing(){
+//        System.out.println("I am singing");
+//    }
+//}
+//
+//public class Inheritance {
+//    public static void main(String[] args) {
+//
+//    }
+//}
