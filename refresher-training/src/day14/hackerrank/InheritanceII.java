a447006e11ee4cef3b3e9c924fead85bbc39ed31
shraddhapal27 <palshraddha41@gmail.com>
2026-08-18 14:40:33 +0530

    [Shraddha Pal] : Added InheritanceII program

--- refresher-training/src/day14/hackerrank/InheritanceII.java
+++ refresher-training/src/day14/hackerrank/InheritanceII.java
@@ -1,4 +1,18 @@
 package day14.hackerrank;
 
+class Arithmetic{
+    int add(int a, int b){
+        return a+b;
+    }
+}
+
+class Adder extends Arithmetic{
+
+}
+
+
 public class InheritanceII {
+    public static void main(String[] args) {
+
+    }
 }
