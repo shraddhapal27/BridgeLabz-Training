975f0dca6cb9f31a0f2123f870504f27850f0d70
shraddhapal27 <palshraddha41@gmail.com>
2026-08-20 23:44:27 +0530

    [Shraddha Pal] : Added hackerrank problems

---
+++ refresher-training/src/day16/hackerrank/Comparator.java
@@ -0,0 +1,12 @@
+package day16.hackerrank;
+
+public class Comparator {
+//    class Checker implements Comparator<Player>{
+//        public int compare(Player p1, Player p2){
+//            if(p1.score != p2.score){
+//                return p2.score - p1.score;
+//            }
+//            return p1.name.compareTo(p2.name);
+//        }
+//    }
+}
---
+++ refresher-training/src/day16/hackerrank/Generics.java
@@ -0,0 +1,5 @@
+package day16.hackerrank;
+
+public class Generics {
+
+}
