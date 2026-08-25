20955262c8e6c670b5d218c6a42c8b0049641f93
shraddhapal27 <palshraddha41@gmail.com>
2026-08-25 14:15:59 +0530

    [Shraddha Pal] : Added ValidUsername program

--- refresher-training/src/day18/hackerrank/ValidUsername.java
+++ refresher-training/src/day18/hackerrank/ValidUsername.java
@@ -1,4 +1,12 @@
 package day18.hackerrank;
 
 public class ValidUsername {
+    public static void main(String[] args) {
+         String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}";
+       if("Ram".matches(regularExpression)){
+           System.out.println("Valid");
+       }else{
+           System.out.println("Invalid");
+       }
+    }
 }
